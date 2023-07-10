cd46bc6114ab169c0fce28472ce38ad516d93845
michelleangela19 <misel.angela79@gmail.com>
2023-07-11 01:57:28 +0700

    pp

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,8 +1,8 @@
-<div class="fixed bg-[#5C9998] top-0 left-0 w-56 h-screen px-3 py-4 overflow-y-auto">
+<div class="navbar-nav fixed bg-[#2C7877] top-0 left-0 w-56 h-screen px-3 py-4 overflow-y-auto">
     <ul class="space-y-2 font-medium">
         <li>
-            <a href="#dataset" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <svg class="w-7 h-7 text-white transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+            <a href="#dataset" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-[#5C9998] group">
+                <svg class="w-7 h-7 text-white transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                     <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
                     <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
                 </svg>
@@ -10,39 +10,39 @@
             </a>
         </li>
         <li>
-            <a href="#container" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <a href="#container" class="flex items-center p-2 text-gray-900 rounded-lg  group hover:bg-[#5C9998]">
                 <svg fill="#ffffff" class="w-7 h-7" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg" stroke="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <defs> <style> .cls-1 { fill: none; } </style> </defs> <title>heat-map--03</title> <path d="M27,3H5A2.0023,2.0023,0,0,0,3,5V27a2.0023,2.0023,0,0,0,2,2H27a2.0023,2.0023,0,0,0,2-2V5A2.0023,2.0023,0,0,0,27,3ZM19,9H13V5h6Zm0,2v4H13V11Zm-8,0v4H5V11Zm0,6v4H5V17Zm2,0h6v4H13Zm8-2V11h6l0,4ZM5,23h6v4H5Zm16,4V23h6v4Z"></path> <rect id="_Transparent_Rectangle_" data-name="<Transparent Rectangle>" class="cls-1" width="32" height="32"></rect> </g></svg>
                 <span class="ml-3 text-white">Heatmap</span>
             </a>
         </li>
         <li>
-        <button type="button" class="flex items-center w-full p-2 text-base text-white transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown">
-                <svg class="flex-shrink-0 w-7 h-7 text-white transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
+        <button type="button" class="flex items-center w-full p-2 text-base text-white transition duration-75 rounded-lg group hover:bg-[#5C9998] " aria-controls="dropdown-example" data-collapse-toggle="dropdown">
+                <svg class="flex-shrink-0 w-7 h-7 text-white transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
                     <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"/>
                 </svg>
-                <span class="flex-1 ml-3 text-left whitespace-nowrap text-white">Relasi</span>
+                <span class="flex-1 ml-3 text-left whitespace-nowrap text-white ">Relasi</span>
                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                 </svg>
         </button>
-        <ul id="dropdown" class="hidden py-2 space-y-2">
+        <ul id="dropdown" class="hidden py-2">
                 <li>
-                    <a href="#agedepress" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Age - Depressed</a>
+                    <a href="#agedepress" class="nav-link flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Age - Depressed</a>
                 </li>
                 <li>
-                    <a href="#edudepress" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Education Level - Depressed</a>
+                    <a href="#edudepress" class="nav-link flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Education Level - Depressed</a>
                 </li>
                 <li>
-                    <a href="#marriagedepress" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Marriage Status - Depresses</a>
+                    <a href="#marriagedepress" class="nav-link flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Marriage Status - Depresses</a>
                 </li>
                 <li>
-                    <a href="#ageedu" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Age - Education Level</a>
+                    <a href="#ageedu" class="nav-link flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Age - Education Level</a>
                 </li>
                 <li>
-                    <a href="#agemarriage" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Age - Marriage Status</a>
+                    <a href="#agemarriage" class="nav-link flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Age - Marriage Status</a>
                 </li>
                 <li>
-                    <a href="#laborincoming" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Labor Primary - Incoming Salary</a>
+                    <a href="#laborincoming" class="nav-link flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Labor Primary - Incoming Salary</a>
                 </li>
         </ul>
         </li>
@@ -50,13 +50,4 @@
 </div>
 
 <style>
-    /* a{
-        text-decoration: none;
-        color: black;
-    }
-
-    a:hover{
-        color: white;
-
-    } */
 </style>
